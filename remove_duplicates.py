9e7f5220dbcbf0f6b3833d51e43e15f1f9668565
robibey <robert.ibey603@gmail.com>
2022-11-18 00:40:06 -0500

    Make comparison fast, handle duplicate filenames

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -1,4 +1,6 @@
 import os
+import time
+from collections import defaultdict
 
 def user_input():
     '''User facing part of the script to determine what function the user
@@ -30,19 +32,24 @@ def find_duplicates(source, dest):
         filepaths.'''
     
 # Walk the source and sub-folders, append path and filename to a list
-    source_files_list = []
+    t1 = time.time()
+    source_files_list = defaultdict(list)
     for src_path, src_dirs, src_files in os.walk(source):
         for src_file in src_files:
-            source_files_list.append(os.path.join(src_path, src_file))
-
+            source_files_list[src_file].append(os.path.join(src_path, src_file))
 # Walk the destination and sub-folders, append path and filename to a list
-    dest_files_list = []
+    dest_files_list = defaultdict(list)
     for dest_path, dest_dirs, dest_files in os.walk(dest):
         for dest_file in dest_files:
-            dest_files_list.append(os.path.join(dest_path, dest_file))
+            dest_files_list[dest_file].append(os.path.join(dest_path, dest_file))
 
-    for x in source_files_list:
-        print(x.split('\\')[-1])
+# Iterate both lists, if dfile = sfile, append sfile to list
+    for item in source_files_list.values():
+        if len(item) > 1:
+            print(f'It appears you have {len(item)} files with the name {item[0].split(chr(92))[-1]}, '
+                'if these are not exact duplicates and just have the same name, please rename the files '
+                f'to have unique names. Here are the locations of all the files: {str(item)[1:-1]}')
+    #print(dest_files_list.keys() & source_files_list.keys())
 #TODO - just compare filenames in the list and return them if they match.
 #TODO - could also compare other things such as modified time but this is mainly to find
 # duplicate images.
